b867f8886c59f9e2fd750d4ea02b84babe1cb001
Chris Bolus <chris.bolus2306@hotmail.com>
2019-04-27 15:55:09 +0100

    Null item scan unit test now passes.

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -6,7 +6,8 @@ namespace CheckoutKata
 {
     public class Checkout : ICheckout
     {
-        private IDataRepository dataRepository;
+        private IDataRepository _dataRepository;
+        private List<string> _items;
 
         /// <summary>
         /// Constructor.  Use dependency injection to get these parameters
@@ -14,7 +15,8 @@ namespace CheckoutKata
         /// <param name="dataRepository">Repository for data needed to calculate the total price</param>
         public Checkout(IDataRepository dataRepository)
         {
-
+            _dataRepository = dataRepository;
+            _items = new List<string>();
         }
 
         public int GetTotalPrice()
@@ -24,6 +26,11 @@ namespace CheckoutKata
 
         public void Scan(string item)
         {
+            if(string.IsNullOrEmpty(item))
+            {
+                //log also
+                throw new ArgumentNullException();
+            }
             throw new NotImplementedException();
         }
     }
