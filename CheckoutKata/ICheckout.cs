4ec1a5832c0cfe7d747fb29476243cc6b0c77151
Chris Bolus <chris.bolus2306@hotmail.com>
2019-04-27 13:28:30 +0100

    Add project files.

---
+++ CheckoutKata/ICheckout.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckoutKata
+{
+    interface ICheckout
+    {
+        void Scan(string item);
+        int GetTotalPrice();
+    }
+}
